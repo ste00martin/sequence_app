74657158a10e1f35e2f693f47315528467c2fc28
Stefan Martin <ste00martin@gmail.com>
2016-01-27 20:46:50 -0800

    adding carousel functionality

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,15 @@
 class StaticPagesController < ApplicationController
-  def products
+  require 'net/http'
 
+  def products
+    url = URI.parse('http://seq-front-end-assessment.s3-website-us-west-2.amazonaws.com/catalog.json')
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    response = JSON.parse(res.body)
+    @products = response['products']
+    @image_base_url = 'http://seq-front-end-assessment.s3-website-us-west-2.amazonaws.com/assets'
   end
+
 end
